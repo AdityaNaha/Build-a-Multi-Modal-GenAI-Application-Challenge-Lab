5ffab96fb51372e3b9f572a90d2ce4b6deafb9b2
AdityaNaha <71839366+AdityaNaha@users.noreply.github.com>
2025-05-13 13:38:00 +0530

    Add files via upload

---
+++ analyze_image.py
@@ -0,0 +1,28 @@
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part
+
+
+def analyze_bouquet_image(project_id: str, location: str) -> str:
+    # Initialize Vertex AI
+    vertexai.init(project=project_id, location=location)
+    # Load the model
+    multimodal_model = GenerativeModel("gemini-2.0-flash-001")
+    # Query the model
+    response = multimodal_model.generate_content(
+        [
+            # Add an example image
+            Part.from_uri(
+                "gs://generativeai-downloads/images/scones.jpg", mime_type="image/jpeg"
+            ),
+            # Add an example query
+            "what is shown in this image?",
+        ]
+    )
+
+    return response.text
+
+project_id = ""
+location = ""
+
+response = analyze_bouquet_image(project_id, location)
+print(response)
